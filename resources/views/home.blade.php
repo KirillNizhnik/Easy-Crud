c2f6aeacfd363ab79ad1d1cc821f1755dc114f61
Kirill Nizhnik <117679943+KirillNizhnik@users.noreply.github.com>
2025-03-01 20:11:02 +0200

    add templates create HomeController

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+    <div class="jumbotron text-center">
+        <h1 class="display-4">Welcome!</h1>
+        <p class="lead">This is the homepage of your Laravel application.</p>
+        <a class="btn btn-primary btn-lg" href="{{ url('/about') }}" role="button">Learn More</a>
+    </div>
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="bg-dark text-light text-center py-3 mt-5">
+    <p>© {{ date('Y') }} Все права защищены</p>
+</footer>
